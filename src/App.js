48f4131132c55484f4735822c83751a7165e71ab
Ashutosh Mishra <ashutoshmishra@Ashutoshs-MacBook-Air.local>
2022-04-13 12:54:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [userInput, setuserInput] = useState({
+    Name : " " ,
+    Usn : " " ,
+    Mob: " ",
+    Age: " ",
+    Address: " "
+  });
+  const handleChange = (event) =>
+  {
+    setuserInput(
+      {
+      [event.target.value] : (event.target.Name),
+      [event.target.value] : (event.target.Usn),
+      [event.target.value] : (event.target.Mob),
+      [event.target.value] : (event.target.Age),
+      [event.target.value] : (event.target.Address)
+
+      })
+  }
+  const [shouldishowvalue, setshouldishowvalue] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>Name</label>
+        <input type= "text" onChange = {handleChange} value = {userInput.Name}/>
+        <label>Usn</label>
+        <input type ="text" onChange = {handleChange} value = {userInput.Usn}/>
+        <label>MOB</label>
+        <input type= "number" onChange = {handleChange} value = {userInput.Mob}/>
+        <label>Age</label>
+        <input type = "number" onChange = {handleChange} value = {userInput.Age}/>
+        <label>Address</label>
+        <input type = "text" onChange = {handleChange} value = {userInput.Address}/>
+       <button onClick= {
+          
+          (
+          <h2>userInput.Name</h2>,
+          <h2>userInput.Usn</h2>,
+          <h2>userInput.Mob</h2>,
+          <h2>userInput.Age</h2>,
+          <h2>userInput.Address</h2>)
+         
+          }>
+        
+          Submit
+        </button>
       </header>
     </div>
   );
